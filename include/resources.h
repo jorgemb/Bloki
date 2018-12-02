3c15dfc2a66eb75c42d5d0a4daafe946e59aa66b
Jorge L. Martinez <jorelmb@gmail.com>
2018-12-02 17:07:54 -0600

    Changed directory structure to a more standard one.

---
+++ include/resources.h
@@ -0,0 +1,47 @@
+// Here are all the resources
+
+/* Icons and others */
+#define	ICON_MAIN	100
+#define CURSOR_MAIN	101
+
+/* IMAGES */
+#define PNG_TYPE "PNG"
+#define IMAGE_TILES	200
+#define GUI_NEWGAME 201
+#define GUI_QUIT 202
+#define GUI_HIGHSCORES 203
+
+#define IMAGE_GAMEOVER 204
+#define IMAGE_YOURNAME 205
+#define IMAGE_SCORE 206
+#define IMAGE_ENTERHIGHSCORE 207
+#define IMAGE_HIGHSCORES 208
+#define IMAGE_TITLE 209
+#define IMAGE_NEXTBLOKUS 210
+
+#define BARO_MESSAGE 211
+
+#define BARO_FIRST 212	// This is to index all baro images
+#define BARO_IMAGES 12
+
+#define BARO_BODY 212
+#define BARO_LEFT_ARM 213
+#define BARO_RIGHT_ARM 214
+#define BARO_LEGS 215
+
+#define BARO_EYES_HAPPY 216
+#define BARO_EYES_NORMAL 217
+#define BARO_EYES_MAD 218
+#define BARO_EYES_WORRIED 219
+
+#define BARO_MOUTH_HAPPY 220
+#define BARO_MOUTH_SAD 221
+#define BARO_MOUTH_WORRIED 222
+#define BARO_MOUTH_O 223
+
+/* Strings */
+#define OFFSET_SPANISH	0
+#define OFFSET_ENGLISH	100
+
+#define ST_CAPTION		0
+
